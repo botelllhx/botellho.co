8b4a5097038d585ff7a5048dd15f0ccf81d937fe
botelllhx <maat9779@gmail.com>
2026-01-17 13:03:50 -0300

    Fix base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // Para GitHub Pages, ajuste o 'base' para o nome do seu repositório
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/botellho.co/' : '/',
+  base: '/',
 })
